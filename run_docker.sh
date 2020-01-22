2354eb938e9ad3116d0793f43ff5a7592616db0d
Mahdi AlKhalaf <mahdi@mahdialkhalaf.com>
2020-01-22 23:05:04 +0300

    feat: Complete the run_docker.sh script for building and running a Docker image

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t sample-housing-prices-ml-api .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run --rm -it --name ml-api -p 8000:80 sample-housing-prices-ml-api
