4b634235e235997922900e43a2d065f91d4fd47c
Mahdi AlKhalaf <mahdi@mahdialkhalaf.com>
2020-01-23 00:57:21 +0300

    feat: Complete the run_kubernetes.sh script for deploying the app to a Kubernetes cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=khalafmh/sample-housing-prices-ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 --image=$dockerpath ml-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/ml-api 8000:80
